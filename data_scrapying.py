a830a9efeb14ae842a40fb5cb80ab945a2833a23
wyjessica860 <wyjessic@umich.edu>
2021-03-21 09:09:53 +0800

    data_collection done

--- data_scrapying.py
+++ data_scrapying.py
@@ -13,42 +13,19 @@ import lyricsgenius
 import sys
 import codecs
 
-"""
-    with open('artists.json','r') as f:
-        artists_dic = json.load(f)
-    artists = artists_dic['artists']
+artists_list = []
+for i in range(1,23):
+    response_artists = requests.get(f"https://www.last.fm/tag/trap+rap/artists?page={i}")
+    re_tree = etree.HTML(response_artists.text)
+    artists_list = artists_list + re_tree.xpath('//*[@class="big-artist-list-title"]/a/text()')
+artists_dic = {}
+artists_dic['artists'] = artists_list
 
-    def itune_term(term):
-        '''
-        input: the term you want to search
-        return a json of the search result
-        '''
-        term = term.replace(" ","+")
-        url = f"https://itunes.apple.com/search?term={term}"
-        response = requests.get(url)
-        if response.status_code != 200:
-            print("The URL is wrong!")
-            return 404
-        return json.loads(response.text)
-    songs_list = []
+with open('artists.json','w') as f:
+    json.dump(artists_dic,f)
 
-    for artist in artists:
-        resp_json = itune_term(artist)
-        if resp_json == 404:
-            continue
-        
-        for i in resp_json["results"]:
-            try : 
-                i["kind"]
-            except:
-                continue
-            if i["kind"] == "song":
-                songs_list.append(i['trackName'])
 
-    songs_dic = {"songs": songs_list}
-    with open('songs.json','w') as f:
-        json.dump(songs_dic,f)
-"""
+
 genius = lyricsgenius.Genius("gulqB5H9cGgsk2Hzo5jX96Q2QAQrMJp2eSF66WUAShkpJxQG8kh0rd1UrJBY8HK5")
 with open('artists.json','r') as f:
     artists_dic = json.load(f)
